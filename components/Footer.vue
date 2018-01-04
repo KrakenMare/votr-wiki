93ffebd22c6f402dd6c539ee0ed25a5025d799d9
Arcanuo <Arcanuo123@gmail.com>
2018-01-04 17:37:04 -0600

    nav

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,12 @@
 <template>
-  <footer>
-    Votr Wiki by <a href="http://arcanuo.me">Arcanuo</a>
-  </footer>
+<footer class="footer">
+  <div class="container">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Votr Wikipedia</strong> by <a href="http://arcanuo.me">Arcanuo</a>. The source code is licensed
+        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content is built for <a href="http//votrproject.com">VotR</a> which is a rotmg pserver.
+      </p>
+    </div>
+  </div>
+</footer>
 </template>
--- pages/legendaries/index.vue
+++ pages/legendaries/index.vue
@@ -4,7 +4,7 @@
     <nav class="navbar is-white">
       <div class="navbar-brand">
         <a class="navbar-item brand-text" href="../">
-          Bulma Blog          
+          Home          
         </a>
         <div class="navbar-burger burger" data-target="navMenu">
           <span></span>
@@ -14,66 +14,42 @@
       </div>
       <div id="navMenu" class="navbar-menu">
         <div class="navbar-start">
-          <a class="navbar-item" href="#">
-            Legendaries
-          </a>
-          <a class="navbar-item" href="#">
-            Dungeons
-          </a>
+          <nuxt-link to="/legendaries" class="navbar-item">Legendaries</nuxt-link>
+              <nuxt-link to="/dungeons" class="navbar-item">Dungeons</nuxt-link>
+
         </div>
       </div>
     </nav>
-    <!-- END NAV -->
-    <section class="articles">
-        <div class="column is-8 is-offset-2">
-
-          <!-- START PROMO BLOCK -->
-          <section class="hero is-danger is-bold is-small promo-block">
-            <div class="hero-body">
-              <div class="container">
-                <h1 class="title">
-                  <i class="fa fa-bell-o"></i> 
-                   Votr Wikipedia
-                </h1>
-                <h2 class="subtitle">
-                  For all your ingame needs. Info on everything in the game.
-                </h2>
+      <div class="column is-center">
+        <div class="box content">
+          <article class="post">
+            <h4>Legendary Dranbiel-Garbs</h4>
+            <span class="pull-right has-text-grey-light"><i class="fa fa-comments"></i> 1</span>
+            <div class="media">
+              <div class="media-left">
+                <p class="image is-32x32">
+                  <img src="http://bulma.io/images/placeholders/128x128.png">
+                </p>
               </div>
-            </div>
-            
-          </section>
-                    <div class="card article">
-            <div class="card-content">
-              <div class="media">
-                <div class="media-content has-text-centered">
-                  <p class="title article-title">How The Wiki Works</p>
-                  <p class="subtitle is-6 article-subtitle">
-                    <a href="http://arcanuo.me">Arcanuo</a>
+              <div class="media-content">
+                <div class="content">
+                  <p>
+                    <nuxt-link to="/dranbiel-garbs" class="navbar-item">Dranbiel Garbs</nuxt-link>
+ 
+                    <span class="tag">Page</span>
                   </p>
                 </div>
               </div>
-
-            <div class="content article-body">
-                <p>The wiki works on two categories as we fill it with data, www.votrproject.com/legendaries/item-name-seperated-by-dashes & www.votrproject.com/dungeons/name.</p>
-                <h3 class="has-text-centered">Rules.</h3>
-                <p> Be considerate of others’ experience.
-Heated competition, profanity, and off-color humor are welcome here. Mean-spirited comments (whether to opponents or teammates), excessive negativity, and a general disregard for others’ experience are not. PURE is a community, and if you are making it an unwelcoming place for others, we’d rather not have you around.
-
-No hate speech.
-Although we are usually concerned with the intent behind language rather than the use of any particular words, we make an exception for language which is primarily associated with hate speech towards a particular demographic. Regardless of intent, the cultural associations with these words makes their usage unconducive to a friendly environment.  Some words result in an automatic kick from our servers; Play safely with others, and attempt to remain friendly. We're all here to play votr.
-
-Do not consciously hinder your fellow players.
-Examples of this include, but are not limited to, intentional dragging , excessive “fooling around”, or refusing to participate in clearing than teleporting to the boss. This is not about player skill; all skill levels are welcome as long as a player is making an honest effort to contribute to their team.
-
-                </p>
-              </div>
             </div>
-          </div>
-
-          </div>
+          </article>
           
-</section>
+        </div>
+      </div>
+
+
 </div>
+    <!-- END NAV -->
+      
 
 </template>
 
@@ -94,4 +70,31 @@ export default {
 </script>
 
 <style scoped>
+.button.is-primary.is-alt {
+  background: #00c6ff;
+  background: -webkit-linear-gradient(to bottom, #0072ff, #00c6ff);
+  background: linear-gradient(to bottom, #0072ff, #00c6ff);
+  font-weight: 700;
+  font-size: 14px;
+  height: 3rem;
+  line-height: 2.8;
+}
+.media-left img {
+  border-radius: 50%;
+}
+.media-content p {
+  font-size: 14px;
+  line-height: 2.3;
+  font-weight: 700;
+  color: #8F99A3;
+}
+article.post {
+  margin: 1rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid #E6EAEE;
+}
+article.post:last-child {
+  padding-bottom: 0;
+  border-bottom: none;
+}
 </style>
