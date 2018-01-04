854de558f688a7eb8356cee0190d79a3211f940c
MrMoisty <35114437+MrMoisty@users.noreply.github.com>
2018-01-04 23:16:03 +0000

    undead-nemesis.vue

---
+++ pages/legendaries/undead-nemesis.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="container">
+    <!-- START NAV -->
+    <nav class="navbar is-white">
+      <div class="navbar-brand">
+                      <nuxt-link to="/" class="navbar-item brand-text">Home</nuxt-link>
+
+        <div class="navbar-burger burger" data-target="navMenu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div id="navMenu" class="navbar-menu">
+        <div class="navbar-start">
+          
+              <nuxt-link to="/legendaries" class="navbar-item">Legendaries</nuxt-link>
+              <nuxt-link to="/dungeons" class="navbar-item">Dungeons</nuxt-link>
+
+          
+        </div>
+      </div>
+    </nav>
+    <!-- END NAV -->
+    <section class="articles">
+        <div class="column is-8 is-offset-2">
+
+          <!-- START PROMO BLOCK -->
+          <section class="hero is-danger is-bold is-small promo-block">
+            <div class="hero-body">
+              <div class="container">
+                <h1 class="title">
+                  <i class="fa fa-bell-o"></i> 
+                   Undead Nemesis
+                </h1>
+                <h2 class="subtitle">
+                  A blade forged by the magic of necromancy and a curse from the evil undead.
+                </h2>
+              </div>
+            </div>
+            
+          </section>
+                    <div class="card article">
+            <div class="card-content">
+             
+            <div class="content article-body">
+                <img src="https://i.imgur.com/sXTyM4H.png" class="image is-128x128" alt="undead nemesis">
+                <h3>Stats.</h3>
+                <p>
+                    <li>Damage: 400-500</li>
+                    <li>Range: 3.5</li>
+                    <li>Ignores defense of target</li>
+                    <li>+100 HP</li>
+                    <li>+8 DEXTERITY</li>
+                    <li>+12 SPEED</li>
+                    <li>Fame Bonus: 8%</li>
+                </p>
+                <h3>How to obtain.</h3>
+                <p>
+                    <li>This sword is forged with the Sword of Dark Necromancy</li>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          </div>
+          
+</section>
+</div>
+
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+export default {
+  async asyncData () {
+    let { data } = await axios.get('/users')
+    return { users: data }
+  },
+  head () {
+    return {
+      title: 'Votr | Legendaries'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
