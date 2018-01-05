ed18da8cb9e478e0b78c8bf53ab14925f783f116
Vanesxa <35114422+Vanesxa@users.noreply.github.com>
2018-01-05 18:17:49 +0100

    Update gargoyle-crusher.vue

--- pages/legendaries/gargoyle-crusher.vue
+++ pages/legendaries/gargoyle-crusher.vue
@@ -44,7 +44,7 @@
             <div class="card-content">
              
             <div class="content article-body">
-                <img src="https://i.imgur.com/rUxrrEs.png" class="image is-128x128" alt="insurgency amulet">
+                <img src="https://i.imgur.com/rUxrrEs.png" class="image is-128x128" alt="gargoyle crusher">
                 <h3>Stats.</h3>
                 <p>
                     <li>Damage: 680-965</li>
