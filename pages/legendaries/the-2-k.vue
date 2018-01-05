cc7bb39fdc274353cfce9f69b8ff685783ae6439
MrMoisty <35114437+MrMoisty@users.noreply.github.com>
2018-01-05 17:16:10 +0000

    the-2-k.vue

---
+++ pages/legendaries/the-2-k.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="container">
+    <!-- START NAV -->
+    <nav class="navbar is-white">
+      <div class="navbar-brand">
+                      <nuxt-link to="/" class="navbar-item brand-text">Home</nuxt-link>
+
+        <div class="navbar-burger burger" data-target="navMenu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div id="navMenu" class="navbar-menu">
+        <div class="navbar-start">
+          
+              <nuxt-link to="/legendaries" class="navbar-item">Legendaries</nuxt-link>
+              <nuxt-link to="/dungeons" class="navbar-item">Dungeons</nuxt-link>
+
+          
+        </div>
+      </div>
+    </nav>
+    <!-- END NAV -->
+    <section class="articles">
+        <div class="column is-8 is-offset-2">
+
+          <!-- START PROMO BLOCK -->
+          <section class="hero is-danger is-bold is-small promo-block">
+            <div class="hero-body">
+              <div class="container">
+                <h1 class="title">
+                  <i class="fa fa-bell-o"></i> 
+                   The 2-K
+                </h1>
+                <h2 class="subtitle">
+                  Created from Genisus shards and assembled by the blacksmith that also resided in the Genisus until he was killed by Oryx because of the creation of this powerful weapon.
+                </h2>
+              </div>
+            </div>
+            
+          </section>
+                    <div class="card article">
+            <div class="card-content">
+             
+            <div class="content article-body">
+                <img src="https://i.imgur.com/gks299q.png" class="image is-128x128" alt="the 2-k">
+                <h3>Stats.</h3>
+                <p>
+                    <li>Damage: 200-2000</li>
+                    <li>Range: 10.1</li>
+                    <li>Shots hit multiple targets</li>
+                    <li>Rate of Fire:-75%</li>
+                    <li>+6 ATTACK</li>
+                    <li>-80 MP</li>
+                    <li>Fame Bonus: 8%</li>
+                </p>
+                <h3>How to obtain.</h3>
+                <p>
+                    <li>This staff is forged with the Shard of Cosmic Collapse</li>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          </div>
+          
+</section>
+</div>
+
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+export default {
+  async asyncData () {
+    let { data } = await axios.get('/users')
+    return { users: data }
+  },
+  head () {
+    return {
+      title: 'Votr | Legendaries'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
